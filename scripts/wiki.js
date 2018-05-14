e485ced1803522fd80441a5e9b45e06085fd2f79
ShotaTakata <papiko.www@gmail.com>
2018-05-14 21:50:35 +0900

    update: wiki検索を実装

---
+++ scripts/wiki.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+
+const options = {
+  method: 'get',
+  url: 'https://ja.wikipedia.org/w/api.php',
+  params: {
+    action: 'query',
+    format: 'json',
+    prop: 'extracts',
+    redirects: 1,
+    exchars: 300,
+    explaintext: 1,
+    titles: ''
+  }
+};
+
+module.exports = (robot) => {
+  robot.respond(/wiki (.+)/i, function (msg) {
+    const keyword = msg.match[1]
+    options.params.titles = encodeURIComponent(keyword)
+    axios(options)
+      .then((res) => {
+        const dynamicKey = Object.keys(res.data.query.pages)[0]
+        const text = res.data.query.pages[dynamicKey].extract
+        return msg.send(`${keyword}は \n\`\`\`${text}\n\`\`\` ということですよ。`)
+      })
+      .catch((err) => {
+        return msg.send(`おや、これはエラーのようですよ。\n\`\`\`\n${err}\n\`\`\``)
+      });
+
+  });
+}
